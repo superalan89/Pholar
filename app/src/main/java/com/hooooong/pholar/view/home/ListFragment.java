1612b35e81b3aeadc7785d34252a48bd684d5fb0
heepie <gheepie@gmail.com>
2017-11-07 14:31:04 +0900

    List 프레그먼트 어뎁터 추가

--- app/src/main/java/com/hooooong/pholar/view/home/ListFragment.java
+++ app/src/main/java/com/hooooong/pholar/view/home/ListFragment.java
@@ -3,6 +3,8 @@ package com.hooooong.pholar.view.home;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +20,18 @@ import java.util.List;
 
 public class ListFragment extends Fragment {
 
+    // 새로운 글을 보여주는 ViewPager와 Adapter
     private ViewPager newWriteViewPager;
-    private NewWriteViewPagerAdapter adapter;
+    private NewWriteViewPagerAdapter newWriteViewPagerAdapter;
+
+    // 친구를 추천해주는 ViewPager와 Adapter
+    private ViewPager recommendFriendViewPager;
+    private RecommendFriendViewPagerAdapter recommendFriendViewPagerAdapter;
+
+    // 게시글을 랜덤으로 보여주는 ViewPager와 Adapter
+    private RecyclerView randomWriteViewPager;
+    private RandomWriteRecyclerViewAdapter randomWriteRecyclerViewAdapter;
+
 
     public ListFragment() {
         // Required empty public constructor
@@ -32,17 +44,9 @@ public class ListFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+        initView(view);
 
-        List<String> data = new ArrayList<>();
 
-        for (int i=0; i<10; i=i+1) {
-            data.add("Happy Day " + i);
-        }
-
-        newWriteViewPager = view.findViewById(R.id.newWriteViewPager);
-        adapter = new NewWriteViewPagerAdapter(view.getContext(), data);
-
-        newWriteViewPager.setAdapter(adapter);
         // Inflate the layout for this fragment
         return view;
 
@@ -51,5 +55,29 @@ public class ListFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    private void initView(View view) {
+        List<String> data = new ArrayList<>();
+
+        for (int i = 0; i < 10; i = i + 1) {
+            data.add("Happy Day " + i);
+        }
+
+        newWriteViewPager = view.findViewById(R.id.newWriteViewPager);
+        recommendFriendViewPager = view.findViewById(R.id.recommedFriendViewPager);
+        randomWriteViewPager = view.findViewById(R.id.randomWriteViewPager);
+
+        // Adapter 생성
+        newWriteViewPagerAdapter = new NewWriteViewPagerAdapter(view.getContext(), data);
+        recommendFriendViewPagerAdapter = new RecommendFriendViewPagerAdapter(view.getContext(), data);
+        randomWriteRecyclerViewAdapter = new RandomWriteRecyclerViewAdapter();
+        randomWriteRecyclerViewAdapter.setDataAndRefresh(data);
+
+        // ViewPager와 Adapter 연결
+        newWriteViewPager.setAdapter(newWriteViewPagerAdapter);
+        recommendFriendViewPager.setAdapter(recommendFriendViewPagerAdapter);
+        randomWriteViewPager.setAdapter(randomWriteRecyclerViewAdapter);
+        randomWriteViewPager.setLayoutManager(new LinearLayoutManager(view.getContext()));
     }
 }
---
+++ app/src/main/java/com/hooooong/pholar/view/home/RandomWriteRecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.hooooong.pholar.view.home;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hooooong.pholar.R;
+
+import java.util.List;
+
+/**
+ * Created by Heepie on 2017. 11. 7..
+ */
+
+public class RandomWriteRecyclerViewAdapter extends RecyclerView.Adapter<RandomWriteRecyclerViewAdapter.Holder> {
+    List<String> data;
+
+    public void setDataAndRefresh(List<String> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.each_write, parent, false);
+
+        Holder holder = new Holder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        String item = data.get(position);
+
+        holder.setItem(item);
+        holder.setDataToScreen();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (data == null)
+            return 0;
+        return data.size();
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+        private String item;
+        private TextView textId;
+
+        public Holder(View itemView) {
+            super(itemView);
+            textId = itemView.findViewById(R.id.each_write_id);
+        }
+
+        public void setItem(String item) {
+            this.item = item;
+        }
+
+        public void setDataToScreen() {
+            textId.setText(item);
+        }
+    }
+}
+
+
