8651cce480e23e827dcfc9e3f35d9297897bedb4
heepie <gheepie@gmail.com>
2017-11-07 11:44:59 +0900

    List 프레그먼트 어뎁터 추가

--- app/src/main/java/com/hooooong/pholar/view/home/ListFragment.java
+++ app/src/main/java/com/hooooong/pholar/view/home/ListFragment.java
@@ -1,17 +1,25 @@
 package com.hooooong.pholar.view.home;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.hooooong.pholar.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Android Hong on 2017-11-06.
  */
 
-public class ListFragment extends android.support.v4.app.Fragment {
+public class ListFragment extends Fragment {
+
+    private ViewPager newWriteViewPager;
+    private NewWriteViewPagerAdapter adapter;
 
     public ListFragment() {
         // Required empty public constructor
@@ -22,7 +30,26 @@ public class ListFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        List<String> data = new ArrayList<>();
+
+        for (int i=0; i<10; i=i+1) {
+            data.add("Happy Day " + i);
+        }
+
+        newWriteViewPager = view.findViewById(R.id.newWriteViewPager);
+        adapter = new NewWriteViewPagerAdapter(view.getContext(), data);
+
+        newWriteViewPager.setAdapter(adapter);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return view;
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 }
